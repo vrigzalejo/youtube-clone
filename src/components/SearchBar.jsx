fa9671810711dc4d70a6e327115a82026cf0b56b
Brigido Alejo <brigsalejo@gmail.com>
2022-10-16 21:24:23 +0800

    Search feed are working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,24 @@ import { Paper, IconButton } from '@mui/material'
 import { Search } from '@mui/icons-material'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`)
+
+      setSearchTerm('')
+    }
+  }
+
   return (
     <Paper
       component='form'
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -16,8 +30,13 @@ const SearchBar = () => {
         mr: { sm: 5 }
       }}
     >
-      <input className='search-bar' placeholder='Search...' value='' onChange={() => {}} />
-      <IconButton type='submit' sx={{ p: '10px' }}>
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
         <Search />
       </IconButton>
     </Paper>
